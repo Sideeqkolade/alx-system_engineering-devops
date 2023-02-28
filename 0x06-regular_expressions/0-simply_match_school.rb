ee91d0747e5cf7f45db694f1143a85f434dc0a4c
Sideeqkolade <sideeqkolade@gmail.com>
2023-02-28 23:16:25 +0100

    tasks

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# The regular expression must match School
+#   4 # Using the project instructions, create a Ruby script·
+#     5 # that accepts one argument and pass it to a regular·
+#       6 # expression matching metputs ARGV[0].scan(/School/)
+puts ARGV[0].scan(/School/)
