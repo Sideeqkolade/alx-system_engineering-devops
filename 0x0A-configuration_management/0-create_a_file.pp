97a818a8caec78ea778e43fc142735c0e949c062
Sideeqkolade <sideeqkolade@gmail.com>
2023-04-24 04:00:36 +0100

    task 0 redone

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,10 @@
 #!/usr/bin/pup
 # create a file in /tmp
+# file path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
 
 file { '/tmp/school':
   content => 'I love Puppet',
